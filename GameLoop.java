91f55a53b6ffba1d129f2ac9ec4526fe7129effe
Zhirou Liu <95239861+Lynnelzr@users.noreply.github.com>
2023-01-06 20:20:02 -0500

    Add files via upload

--- GameLoop.java
+++ GameLoop.java
@@ -446,7 +446,9 @@ public class GameLoop {
                                     } else if (decision_floorlamp.equals("shut off the fireplace")) {
                                         livingroom.ShutOffFireplace();
                                         System.out.println();
-                                    } else {//When the user inputs unexpected message, print a default invalid input message.
+                                    } else if (decision_floorlamp.equals(" ")){
+                                        break;
+                                    }else {//When the user inputs unexpected STARmessage, print a default invalid input message.
                                         InvalidInput();
                                         System.out.println();
                                     }
@@ -462,7 +464,7 @@ public class GameLoop {
                                         livingroom.switchChannel();
                                         System.out.println();
 
-                                    } else if (decision_TV.equals("stop watching and turn off tv")) {
+                                    } else if (decision_TV.equals("stop watching and turn off the tv")) {
                                         livingroom.turnOnOff_TV("off");
                                         System.out.println();
                                         stillSearching = true;
